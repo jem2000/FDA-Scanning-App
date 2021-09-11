a758b19396be16ffd35ce3fa7253a841a288bf98
jlam8080 <jlam8080@bu.edu>
2021-09-11 16:21:15 -0400

    Copy of working Barcode Button

---
+++ Scanner_to_Calorie_Navigate/components/BarcodeScanner.js
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+
+import {BarCodeScanner} from 'expo-barcode-scanner';
+import { AsyncStorage } from "react-native";
+
+import { useNavigation } from '@react-navigation/native';
+
+
+
+export default class BarcodeScanner extends React.Component {
+  // const [hasPermission, setHasPermission] = useState(null);
+  // const [scanned, setScanned] = useState(false);
+  state = { scanned: false }
+  // const nav = useNavigation();
+  //
+  // console.log("scan2");
+  //
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await BarCodeScanner.requestPermissionsAsync();
+  //     setHasPermission(status === 'granted');
+  //   })();
+  // }, []);
+  render() {
+
+    let fdc = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=TpuNpCiJzfPy5vcMvI9xP2yBwowcY8QqVpCus6De&query=';
+    let code = '04976400'; //sprite
+
+    async function getFDC(link) {
+      let response = await fetch(link);
+      return await response.json();
+    }
+
+    const handleBarCodeScanned = ({type, data}) => {
+      this.state.scanned = true;
+      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+      getFDC(fdc + data).then(fdc_data => {
+            console.log(fdc_data);
+            console.log(fdc_data.foods[0].description);
+            console.log(fdc_data.foods[0].foodNutrients[3].value);
+          }
+      );
+    };
+
+    // if (hasPermission === null) {
+    //   return <Text>Requesting for camera permission</Text>;
+    // }
+    // if (hasPermission === false) {
+    //   return <Text>No access to camera</Text>;
+    // }
+    return (
+        <View style={styles.container}>
+          <BarCodeScanner
+              onBarCodeScanned={this.state.scanned ? undefined : handleBarCodeScanned}
+              style={StyleSheet.absoluteFillObject}
+          />
+          {this.state.scanned && <Button title={'Tap to Scan Again'} onPress={() => this.state.scanned = false}/>}
+          <Button title="Exit" onPress={() => {
+            this.props.navigation.navigate("Main")
+          }}/>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
---
+++ Scanner_to_Calorie_Navigate/components/Main.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  StyleSheet,
+  Iconoi,
+  Button,
+  TextInput,
+  Text,
+  View
+} from "react-native";
+import * as firebase from "firebase";
+import { AsyncStorage } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+
+export default class Main extends React.Component {
+  state = { currentUser: null };
+  componentDidMount() {
+    const { currentUser } = firebase.auth();
+    this.setState({ currentUser });
+  }
+  logoutUser = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // AsyncStorage.removeItem("key");
+        this.props.navigation.navigate("Login");
+      });
+  };
+  render() {
+    const { currentUser } = this.state;
+    return (
+        <View  style={styles.container}>
+           <Text>Hi {currentUser && currentUser.email}!</Text>
+           <Button title="Logout" onPress={this.logoutUser} />
+           <Button title = "Scan Barcode" onPress={() => this.props.navigation.navigate("BarcodeScanner")} />
+        </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
