3b7788396875736ccbfcffabc940162a51cba5b5
Justin Melville <justinem2000@gmail.com>
2021-09-11 15:34:33 -0400

    Converted barcodeScanner to a class
    Exit works now, must fix camera permissions

--- Food_scanner_and_calorie_counter/components/BarcodeScanner.js
+++ Food_scanner_and_calorie_counter/components/BarcodeScanner.js
@@ -4,55 +4,65 @@ import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import {BarCodeScanner} from 'expo-barcode-scanner';
 import { AsyncStorage } from "react-native";
 
+import { useNavigation } from '@react-navigation/native';
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  console.log("scan2");
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
 
-  let fdc = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=TpuNpCiJzfPy5vcMvI9xP2yBwowcY8QqVpCus6De&query=';
-  let code = '04976400'; //sprite
+export default class BarcodeScanner extends React.Component {
+  // const [hasPermission, setHasPermission] = useState(null);
+  // const [scanned, setScanned] = useState(false);
+  state = { scanned: false }
+  // const nav = useNavigation();
+  //
+  // console.log("scan2");
+  //
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await BarCodeScanner.requestPermissionsAsync();
+  //     setHasPermission(status === 'granted');
+  //   })();
+  // }, []);
+  render() {
 
-  async function getFDC(link) {
-    let response = await fetch(link);
-    return await response.json();
-  }
+    let fdc = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=TpuNpCiJzfPy5vcMvI9xP2yBwowcY8QqVpCus6De&query=';
+    let code = '04976400'; //sprite
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-
-    getFDC(fdc + data).then(fdc_data => {
-      console.log(fdc_data);
-      console.log(fdc_data.foods[0].description);
-      console.log(fdc_data.foods[0].foodNutrients[3].value);
+    async function getFDC(link) {
+      let response = await fetch(link);
+      return await response.json();
     }
-  );
-  };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+    const handleBarCodeScanned = ({type, data}) => {
+      this.state.scanned = true;
+      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+      getFDC(fdc + data).then(fdc_data => {
+            console.log(fdc_data);
+            console.log(fdc_data.foods[0].description);
+            console.log(fdc_data.foods[0].foodNutrients[3].value);
+          }
+      );
+    };
+
+    // if (hasPermission === null) {
+    //   return <Text>Requesting for camera permission</Text>;
+    // }
+    // if (hasPermission === false) {
+    //   return <Text>No access to camera</Text>;
+    // }
     return (
         <View style={styles.container}>
           <BarCodeScanner
-              onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+              onBarCodeScanned={this.state.scanned ? undefined : handleBarCodeScanned}
               style={StyleSheet.absoluteFillObject}
           />
-          {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)}/>}
-          <Button title="Exit" onPress={() => this.props.navigation.navigate("Main")}/>
+          {this.state.scanned && <Button title={'Tap to Scan Again'} onPress={() => this.state.scanned = false}/>}
+          <Button title="Exit" onPress={() => {
+            this.props.navigation.navigate("Main")
+          }}/>
         </View>
     );
+  }
 }
 
 const styles = StyleSheet.create({
