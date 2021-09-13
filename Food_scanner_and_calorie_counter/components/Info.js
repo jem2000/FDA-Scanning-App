e77e17c235fa07fad8f1d5c71fd8709f695b9391
Justin Melville <justinem2000@gmail.com>
2021-09-13 02:12:30 -0400

    Fixed Android timer warning

--- Food_scanner_and_calorie_counter/components/Info.js
+++ Food_scanner_and_calorie_counter/components/Info.js
@@ -3,6 +3,17 @@ import { Image, StyleSheet, Text, TouchableOpacity, View, ScrollView, Button, Te
 import * as firebase from "firebase";
 
 import Constants from 'expo-constants';
+import { LogBox } from 'react-native';
+import _ from 'lodash';
+
+LogBox.ignoreLogs(['Warning:...']); // ignore specific logs
+LogBox.ignoreAllLogs(); // ignore all logs
+const _console = _.clone(console);
+console.warn = message => {
+    if (message.indexOf('Setting a timer') <= -1) {
+        _console.warn(message);
+    }
+};
 
 let FoodItem = {
   Title: "",
